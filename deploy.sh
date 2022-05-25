2fe1720d0d02c88a34ffdbad2aea4926757adec1
fossephate <matt.cfosse@gmail.com>
2022-05-25 17:44:14 -0400

    python fixes, minor folder re-structuring

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build . -t fossephate/nautilus-backend
-sudo docker push fossephate/nautilus-backend:latest
-
-doctl compute domain records create perish.co --record-type "A" --record-name "backend" --record-data "45.55.125.226" --record-ttl "30"
---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+sudo docker build . -t fossephate/nautilus-backend
+sudo docker push fossephate/nautilus-backend:latest
+
+# DNS:
+doctl compute domain records create perish.co --record-type "A" --record-name "nautilus" --record-data "45.55.125.226" --record-ttl "30"
+
+doctl compute domain records list perish.co
+doctl compute domain records create perish.co --record-type "A" --record-name "backend" --record-data "45.55.125.226" --record-ttl "30"
+
+
+doctl compute domain records create perish.co --record-type "CNAME" --record-name "test-app" --record-data "custom.bnc.lt" --record-ttl "30"
+doctl compute domain records create perish.co --record-type "CNAME" --record-name "app" --record-data "custom.bnc.lt" --record-ttl "30"
